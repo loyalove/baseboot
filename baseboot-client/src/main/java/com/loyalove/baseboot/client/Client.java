3611301b80d26019abdaeb1a882ec00ab311e24c
loyalove <598733638@qq.com>
2016-12-05 18:02:40 +0800

    日志格式优化调整

--- baseboot-client/src/main/java/com/loyalove/baseboot/client/Client.java
+++ baseboot-client/src/main/java/com/loyalove/baseboot/client/Client.java
@@ -13,6 +13,6 @@ public class Client {
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(Client.class, args);
         EchoServiceClient bean = run.getBean(EchoServiceClient.class);
-        System.out.println(bean.echo("abccc"));
+        System.out.println(bean.echo("abc"));
     }
 }
